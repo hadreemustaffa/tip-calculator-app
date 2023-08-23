6e68b8703c312527dda33a6854364ba1d6d2da3f
drevkr <hadree.mustaffa@gmail.com>
2023-08-23 18:18:43 +0800

    add variables

---
+++ index.js
@@ -0,0 +1,10 @@
+const bill = document.getElementById('billAmount');
+const setTip = document.querySelectorAll('div.tip-input-container > input');
+const people = document.getElementById('numberOfPeople');
+const tipPerPerson = document.getElementById('tip-per-person');
+const totalPerPerson = document.getElementById('total-per-person');
+const reset = document.getElementById('reset');
+
+function calculateTip() {}
+
+calculateTip();
