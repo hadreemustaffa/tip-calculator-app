44339f311f5cb2a82bee00cec156d88671f7868b
drevkr <hadree.mustaffa@gmail.com>
2023-08-25 15:47:11 +0800

    add calculation. TODO: media queries, refactor JS

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-const bill = document.getElementById('billAmount');
-const setTip = document.querySelectorAll('div.tip-input-container > input');
-const people = document.getElementById('numberOfPeople');
-const tipPerPerson = document.getElementById('tip-per-person');
-const totalPerPerson = document.getElementById('total-per-person');
-const reset = document.getElementById('reset');
+const calculateTip = (formSelector, outputSelector) => {
+	const formElement = document.querySelector(formSelector);
+	const outputElement = document.querySelector(outputSelector);
+	const tipButtons = document.querySelectorAll('.tip-btn');
+	const amountBill = document.getElementById('billAmount');
+	const totalPeople = document.getElementById('numberOfPeople');
+	const tipPerson = document.getElementById('tip-per-person');
+	const totalPerson = document.getElementById('total-per-person');
 
-function calculateTip() {}
+	for (let i = 0; i < tipButtons.length; i++) {
+		formElement.addEventListener('keyup', () => {
+			let bill = parseInt(amountBill.value);
+			let people = parseInt(totalPeople.value);
 
-calculateTip();
+			tipButtons[i].addEventListener('click', () => {
+				let tip = parseFloat(tipButtons[i].value);
+				let calcTip = (bill * (tip / 100)) / people;
+				let calcTotal = (bill + calcTip * people) / people;
+				tipPerson.innerHTML = calcTip.toFixed(2);
+				totalPerson.innerHTML = calcTotal.toFixed(2);
+			});
+		});
+
+		tipButtons.forEach((button) => {
+			button.addEventListener('click', () => {
+				document.querySelector('.active')?.classList.remove('active');
+				button.classList.add('active');
+			});
+			button.addEventListener('blur', () => {
+				button.classList.remove('active');
+			});
+		});
+	}
+
+	formElement.setAttribute('novalidate', '');
+
+	// reset all inputs on click
+	function reset() {
+		const resetBtn = document.getElementById('reset');
+		resetBtn.addEventListener('click', () => {
+			formElement.reset();
+			outputElement.reset();
+		});
+	}
+
+	reset();
+};
+
+calculateTip('#form', '.output-cost');
